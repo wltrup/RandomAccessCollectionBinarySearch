12f9fdce9be24a6c70c451532ddb258adef0a56f
Wagner Truppel <trupwl@gmail.com>
2020-07-12 15:32:58 +0100

    WIP

--- Sources/RandomAccessCollectionBinarySearch/RandomAccessCollectionBinarySearch.swift
+++ Sources/RandomAccessCollectionBinarySearch/RandomAccessCollectionBinarySearch.swift
@@ -1,3 +1,351 @@
-struct RandomAccessCollectionBinarySearch {
-    var text = "Hello, World!"
+import Foundation
+
+extension RandomAccessCollection where Element: Comparable {
+
+    /// Returns the index of the target element, if it exists in the
+    /// input collection, or `nil`, if it does not. It is assumed that
+    /// the collection is sorted.
+    public static func binarySearch(in input: Self, for targetElement: Element) -> Index? {
+        let (_, targetIndex, _) = input.binarySearch(for: targetElement)
+        return targetIndex
+    }
+
+    // --- //
+
+    /// Returns the index of the target element, if it exists in the
+    /// input collection (`self`), or `nil`, if it does not. It is
+    /// assumed that the collection is sorted.
+    public func binarySearch(for targetElement: Element) -> Index? {
+        Self.binarySearch(in: self, for: targetElement)
+    }
+
+    // ---------- //
+
+    /// Returns a tuple containing the **indices** of the two collection elements
+    /// that narrowly bracket the target element in the input collection
+    /// (**assumed sorted**), as well as the **index** of the target element itself.
+    /// Note that the index of the target element appears **in the middle**, in
+    /// the returned tuple `(low, target, high)`, and these three indices are *always*
+    /// in *ascending order*, regardless of the sorting order of the input collection.
+    ///
+    /// The only possible combinations of these indices are:
+    ///
+    ///   - The collection is *empty*, in which case
+    ///
+    ///     * `(low, target, high) = (nil, nil, nil)`.
+    ///
+    ///   - The collection has exactly *one element*, in which case
+    ///
+    ///     * `(low, target, high) = (nil, nil, 0)`, if the target element is *smaller*
+    ///       than the collection's single element.
+    ///
+    ///     * `(low, target, high) = (nil, 0, nil)`, if the target element is *equal*
+    ///       to the collection's single element.
+    ///
+    ///     * `(low, target, high) = (0, nil, nil)`, if the target element is *larger*
+    ///       than the collection's single element.
+    ///
+    ///   - The collection has *at least two* elements, in which case
+    ///
+    ///     * `(low, target, high) = (nil, nil, z)`, if the collection is sorted
+    ///       in *ascending order*, the target element *is not* in the collection,
+    ///       and it is *smaller than* the collection's *smallest* element. `z` is
+    ///       then the index of the collection's *smallest* element.
+    ///
+    ///     * `(low, target, high) = (nil, y, z)`, if the collection is sorted
+    ///       in *ascending order*, the target element *is* in the collection,
+    ///       and it *equals* the collection's *smallest* element. `y` is then
+    ///       the found index of the target element and `z` is the index of the
+    ///       element *just after* that (the *smallest* element that is still
+    ///       *larger* than the target element).
+    ///
+    ///     * `(low, target, high) = (x, y, z)`, if the collection is sorted
+    ///       in *ascending order*, the target element *is* in the collection,
+    ///       and it equals some *intermediate* element. `y` is then the found
+    ///       index of the target element, `x` is the index of the element
+    ///       *just before* that (the *largest* element that is still *smaller*
+    ///       than the target element) and `z` is the index of the element
+    ///       *just after* the target element (the *smallest* element that is
+    ///       still *larger* than the target element).
+    ///
+    ///     * `(low, target, high) = (x, y, nil)`, if the collection is sorted
+    ///       in *ascending order*, the target element *is* in the collection,
+    ///       and it *equals* the collection's *largest* element. `y` is then
+    ///       the found index of the target element and `x` is the index of
+    ///       the element *just before* that (the *largest* element that is
+    ///       still *smaller* than the target element).
+    ///
+    ///     * `(low, target, high) = (x, nil, nil)`, if the collection is sorted
+    ///       in *ascending order*, the target element *is not* in the collection,
+    ///       and it is *larger than* the collection's *largest* element. `x` is
+    ///       then the index of the collection's *largest* element.
+    ///
+    /// For collections with integer indices, `y = x+1` and `z = y+1`.
+    ///
+    public static func binarySearch(in input: Self, for targetElement: Element)
+        -> (low: Index?, target: Index?, high: Index?) {
+            let (lowIndex, targetIndex, highIndex) = input.binarySearch(for: targetElement)
+            return (lowIndex, targetIndex, highIndex)
+    }
+
+    // --- //
+
+    /// Returns a tuple containing the two indices that narrowly bracket the target element
+    /// in the input collection (`self`, **assumed sorted**), as well as the index of the
+    /// target element itself. Note that the index of the target element appears
+    /// **in the middle**, in the returned tuple `(low, target, high)`.
+    ///
+    public func binarySearch(for targetElement: Element)
+        -> (low: Index?, target: Index?, high: Index?) {
+
+        guard isEmpty == false else { return (nil, nil, nil) }
+
+        guard count > 1 else {
+
+            // target element found
+            if targetElement == self[startIndex] {
+                return (nil, startIndex, nil)
+            }
+
+            // target element not found
+            if targetElement < self[startIndex] {
+                return (nil, nil, startIndex)
+            } else {
+                return (startIndex, nil, nil)
+            }
+
+        }
+
+        let firstValidIndex = startIndex
+        let lastValidIndex = index(before: endIndex)
+
+        let isSortedAscending = self[lastValidIndex] > self[firstValidIndex]
+
+        var lowerIndex = firstValidIndex
+        var upperIndex = lastValidIndex
+
+        if isSortedAscending {
+
+            while (true) {
+
+                let upperMinusLower = distance(from: lowerIndex, to: upperIndex)
+
+                if upperMinusLower == 0 { // upperIndex == lowerIndex
+
+                    // is the index pointing to the first element?
+                    if lowerIndex == firstValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the first element
+                            return (nil, index(after: firstValidIndex), firstValidIndex)
+                        } else {
+                            // target element not found
+                            return (nil, nil, firstValidIndex)
+                        }
+                    }
+
+                    // is the index pointing to the last element?
+                    if lowerIndex == lastValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the last element
+                            return (index(before: lastValidIndex), nil, lastValidIndex)
+                        } else {
+                            // target element not found
+                            return (lastValidIndex, nil, nil)
+                        }
+                    }
+
+                    // the index is pointing to an intermediate element
+                    if self[lowerIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (index(before: lowerIndex), index(after: lowerIndex), lowerIndex)
+                    } else {
+                        // target element not found but it is bracketed
+                        if self[lowerIndex] < targetElement {
+                            return (lowerIndex, index(after: lowerIndex), nil)
+                        } else {
+                            return (index(before: lowerIndex), lowerIndex, nil)
+                        }
+                    }
+
+                }
+
+                if upperMinusLower == 1 { // upperIndex == lowerIndex + 1
+
+                    // is lowerIndex pointing to the first element?
+                    if lowerIndex == firstValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the first element
+                            return (nil, firstValidIndex, upperIndex)
+                        } else if self[upperIndex] == targetElement {
+                            // target element found and it is the second element
+                            return (firstValidIndex, index(after: upperIndex), upperIndex)
+                        } else {
+                            // target element not found
+                            return (nil, nil, firstValidIndex)
+                        }
+                    }
+
+                    // is upperIndex pointing to the last element?
+                    if upperIndex == lastValidIndex {
+                        if self[upperIndex] == targetElement {
+                            // target element found and it is the last element
+                            return (lowerIndex, lastValidIndex, nil)
+                        } else if self[lowerIndex] == targetElement {
+                            // target element found and it is the next-to-last element
+                            return (index(before: lowerIndex), lastValidIndex, lowerIndex)
+                        } else {
+                            // target element not found
+                            return (lastValidIndex, nil, nil)
+                        }
+                    }
+
+                    // lowerIndex and upperIndex are pointing to adjacent elements
+                    // that are not at the edges of the input collection
+
+                    if self[lowerIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (index(before: lowerIndex), upperIndex, lowerIndex)
+                    }
+
+                    if self[upperIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (lowerIndex, index(after: upperIndex), upperIndex)
+                    }
+
+                    // target element not found but it is bracketed
+                    return (lowerIndex, nil, upperIndex)
+
+                }
+
+                // upperMinusLower > 1
+
+                let currentIndex = self.index(lowerIndex, offsetBy: upperMinusLower / 2)
+
+                if self[currentIndex] == targetElement {
+                    // target element found and it is an intermediate element
+                    return (index(before: currentIndex), index(after: currentIndex), currentIndex)
+                }
+
+                if (self[currentIndex] > targetElement) {
+                    upperIndex = self.index(before: currentIndex)
+                } else {
+                    lowerIndex = self.index(after: currentIndex)
+                }
+
+            }
+
+        } else {
+
+            while (true) {
+
+                let upperMinusLower = distance(from: lowerIndex, to: upperIndex)
+
+                if upperMinusLower == 0 { // upperIndex == lowerIndex
+
+                    // is the index pointing to the first element?
+                    if lowerIndex == firstValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the first element
+                            return (nil, index(after: firstValidIndex), firstValidIndex)
+                        } else {
+                            // target element not found
+                            return (nil, nil, firstValidIndex)
+                        }
+                    }
+
+                    // is the index pointing to the last element?
+                    if lowerIndex == lastValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the last element
+                            return (index(before: lastValidIndex), nil, lastValidIndex)
+                        } else {
+                            // target element not found
+                            return (lastValidIndex, nil, nil)
+                        }
+                    }
+
+                    // the index is pointing to an intermediate element
+                    if self[lowerIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (index(before: lowerIndex), index(after: lowerIndex), lowerIndex)
+                    } else {
+                        // target element not found but it is bracketed
+                        if self[lowerIndex] < targetElement {
+                            return (lowerIndex, index(after: lowerIndex), nil)
+                        } else {
+                            return (index(before: lowerIndex), lowerIndex, nil)
+                        }
+                    }
+
+                }
+
+                if upperMinusLower == 1 { // upperIndex == lowerIndex + 1
+
+                    // is lowerIndex pointing to the first element?
+                    if lowerIndex == firstValidIndex {
+                        if self[lowerIndex] == targetElement {
+                            // target element found and it is the first element
+                            return (nil, firstValidIndex, upperIndex)
+                        } else if self[upperIndex] == targetElement {
+                            // target element found and it is the second element
+                            return (firstValidIndex, index(after: upperIndex), upperIndex)
+                        } else {
+                            // target element not found
+                            return (nil, nil, firstValidIndex)
+                        }
+                    }
+
+                    // is upperIndex pointing to the last element?
+                    if upperIndex == lastValidIndex {
+                        if self[upperIndex] == targetElement {
+                            // target element found and it is the last element
+                            return (lowerIndex, lastValidIndex, nil)
+                        } else if self[lowerIndex] == targetElement {
+                            // target element found and it is the next-to-last element
+                            return (index(before: lowerIndex), lastValidIndex, lowerIndex)
+                        } else {
+                            // target element not found
+                            return (lastValidIndex, nil, nil)
+                        }
+                    }
+
+                    // lowerIndex and upperIndex are pointing to adjacent elements
+                    // that are not at the edges of the input collection
+
+                    if self[lowerIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (index(before: lowerIndex), upperIndex, lowerIndex)
+                    }
+
+                    if self[upperIndex] == targetElement {
+                        // target element found and it is an intermediate element
+                        return (lowerIndex, index(after: upperIndex), upperIndex)
+                    }
+
+                    // target element not found but it is bracketed
+                    return (lowerIndex, nil, upperIndex)
+
+                }
+
+                // upperMinusLower > 1
+
+                let currentIndex = self.index(lowerIndex, offsetBy: upperMinusLower / 2)
+
+                if self[currentIndex] == targetElement {
+                    // target element found and it is an intermediate element
+                    return (index(before: currentIndex), index(after: currentIndex), currentIndex)
+                }
+
+                if (self[currentIndex] > targetElement) {
+                    upperIndex = self.index(before: currentIndex)
+                } else {
+                    lowerIndex = self.index(after: currentIndex)
+                }
+
+            }
+
+        }
+
+    }
+
 }
--- Tests/RandomAccessCollectionBinarySearchTests/RandomAccessCollectionBinarySearchTests.swift
+++ Tests/RandomAccessCollectionBinarySearchTests/RandomAccessCollectionBinarySearchTests.swift
@@ -2,14 +2,127 @@ import XCTest
 @testable import RandomAccessCollectionBinarySearch
 
 final class RandomAccessCollectionBinarySearchTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RandomAccessCollectionBinarySearch().text, "Hello, World!")
+
+    func test_empty_collection() {
+
+        let input: [Int] = []
+
+        let (lowIndex, targetIndex, highIndex) = input.binarySearch(for: 5)
+
+        XCTAssertEqual(lowIndex, nil)
+        XCTAssertEqual(targetIndex, nil)
+        XCTAssertEqual(highIndex, nil)
+
+    }
+
+    func test_single_element_collection() {
+
+        let input = [5]
+
+        var (lowIndex, targetIndex, highIndex) = input.binarySearch(for: 1)
+        XCTAssertEqual(lowIndex, nil)
+        XCTAssertEqual(targetIndex, nil)
+        XCTAssertEqual(highIndex, 0)
+
+        (lowIndex, targetIndex, highIndex) = input.binarySearch(for: 5)
+        XCTAssertEqual(lowIndex, nil)
+        XCTAssertEqual(targetIndex, 0)
+        XCTAssertEqual(highIndex, nil)
+
+        (lowIndex, targetIndex, highIndex) = input.binarySearch(for: 10)
+        XCTAssertEqual(lowIndex, 0)
+        XCTAssertEqual(targetIndex, nil)
+        XCTAssertEqual(highIndex, nil)
+
+    }
+
+    func test_general_size_collection_ascending() {
+
+        // [1, 2, 3, 4, 6, 7, 8, 9] - missing 5
+        let input = Array(1 ..< 5) + Array(6 ..< 10)
+
+        // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for targetElement in Array(0 ... 10) {
+            let (lowIndex, targetIndex, highIndex) = input.binarySearch(for: targetElement)
+            if targetElement == 0 {
+                XCTAssertEqual(lowIndex, nil)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, 0)
+            } else if targetElement == 1 {
+                XCTAssertEqual(lowIndex, nil)
+                XCTAssertEqual(targetIndex, 0)
+                XCTAssertEqual(highIndex, 1)
+            } else if targetElement == 5 {
+                XCTAssertEqual(lowIndex, 3)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, 4)
+            } else if targetElement == 9 {
+                XCTAssertEqual(lowIndex, 6)
+                XCTAssertEqual(targetIndex, 7)
+                XCTAssertEqual(highIndex, nil)
+            } else if targetElement == 10 {
+                XCTAssertEqual(lowIndex, 7)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, nil)
+            } else if targetElement < 5 {
+                XCTAssertEqual(lowIndex, targetElement - 2)
+                XCTAssertEqual(targetIndex, targetElement - 1)
+                XCTAssertEqual(highIndex, targetElement)
+            } else if targetElement < 10 {
+                XCTAssertEqual(lowIndex, targetElement - 3)
+                XCTAssertEqual(targetIndex, targetElement - 2)
+                XCTAssertEqual(highIndex, targetElement - 1)
+            }
+        }
+
+    }
+
+    func test_general_size_collection_descending() {
+
+        // [9, 8, 7, 6, 4, 3, 2, 1] - missing 5
+        let input: [Int] = (Array(1 ..< 5) + Array(6 ..< 10)).reversed()
+
+        // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for targetElement in Array(0 ... 10) {
+            let (lowIndex, targetIndex, highIndex) = input.binarySearch(for: targetElement)
+            if targetElement == 0 {
+                XCTAssertEqual(lowIndex, nil)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, 0)
+            } else if targetElement == 1 {
+                XCTAssertEqual(lowIndex, nil)
+                XCTAssertEqual(targetIndex, 0)
+                XCTAssertEqual(highIndex, 1)
+            } else if targetElement == 5 {
+                XCTAssertEqual(lowIndex, 3)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, 4)
+            } else if targetElement == 9 {
+                XCTAssertEqual(lowIndex, 6)
+                XCTAssertEqual(targetIndex, 7)
+                XCTAssertEqual(highIndex, nil)
+            } else if targetElement == 10 {
+                XCTAssertEqual(lowIndex, 7)
+                XCTAssertEqual(targetIndex, nil)
+                XCTAssertEqual(highIndex, nil)
+            } else if targetElement < 5 {
+                XCTAssertEqual(lowIndex, targetElement - 2)
+                XCTAssertEqual(targetIndex, targetElement - 1)
+                XCTAssertEqual(highIndex, targetElement)
+            } else if targetElement < 10 {
+                XCTAssertEqual(lowIndex, targetElement - 3)
+                XCTAssertEqual(targetIndex, targetElement - 2)
+                XCTAssertEqual(highIndex, targetElement - 1)
+            }
+        }
+
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test_empty_collection", test_empty_collection),
+        ("test_single_element_collection", test_single_element_collection),
+        ("test_general_size_collection_ascending", test_general_size_collection_ascending),
+        ("test_general_size_collection_descending", test_general_size_collection_descending),
     ]
+
 }
